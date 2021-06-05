6511f75bd2c827ea86c8faa20b7e6da4ba59a739
Dhruvi0901 <79635333+Dhruvi0901@users.noreply.github.com>
2021-06-05 22:53:29 +0530

    Add files via upload

---
+++ Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header className="header">
+      <div>
+        <h1>Learning JavaScript</h1>
+      </div>
+    </header>
+  );
+}
+export default Header;
